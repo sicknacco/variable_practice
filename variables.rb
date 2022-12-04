7919f8e86c28217156b90ea7a8ced2d40264d6ef
Nick Sacco <nicksacco10@gmail.com>
2022-12-04 07:35:18 -0800

    Add integer variables

--- variables.rb
+++ variables.rb
@@ -1,4 +1,10 @@
 # Variables assigned to Strings
-- email
-- name
-- address
+- email = "nicksacco10@gmail.com"
+- name = "Nick"
+- address = 4530 1/2 32nd Street
+
+# Variables assigned to Integers
+- age = 32
+- number_of_dogs = 1
+- assignments_completed = 5
+
